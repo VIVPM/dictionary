db5589fee8e59361358c11f1615ad591b5fb6e67
VIVEK PM <vivpm99@gmail.com>
2023-05-08 13:27:58 +0530

    dict

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Dict from './Dict';
+
+function App() {
+  return (
+    <div className="App">
+      <Dict/>
+    </div>
+  );
+}
+
+export default App;
